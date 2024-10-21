d25c75428b4255359bacd72dc0692b685cce21d9
tyukavkin99 <tyukavkin99@gmail.com>
2024-10-21 18:16:35 +0100

    add pipeline train

---
+++ src/CarPricePred/pipeline/train_model.py
@@ -0,0 +1,11 @@
+from src.CarPricePred.components.data_ingestion import DataIngestion
+from src.CarPricePred.components.data_transformation import DataTransformer
+from src.CarPricePred.components.model_trainer import Model_Trainer
+from sklearn import pipeline
+
+
+def main():
+    pass
+
+if __name__ == "main":
+    main()
