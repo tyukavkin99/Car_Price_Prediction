b6be61d9f081158e7d0a93867088efb98c31c383
tyukavkin99 <tyukavkin99@gmail.com>
2024-10-22 17:37:53 +0100

    add logger exception

--- src/CarPricePred/logger.py
+++ src/CarPricePred/logger.py
@@ -10,5 +10,9 @@ LOG_FILE_PATH = os.path.join(logs_path, LOG_FILE)
 
 logging.basicConfig(
     filename=LOG_FILE_PATH,
-    format=[]
+    format="[ %(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s",
+    level=logging.INFO
 )
+
+if __name__ == "__main__":
+    logging.info("Logging has started")
