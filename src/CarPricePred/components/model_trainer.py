fe8a4b25ace4d523e9980729a6a7e679b1d829e6
tyukavkin99 <tyukavkin99@gmail.com>
2024-10-25 21:57:21 +0100

    add model_trainer

--- src/CarPricePred/components/model_trainer.py
+++ src/CarPricePred/components/model_trainer.py
@@ -4,18 +4,15 @@ from sklearn.linear_model import LinearRegression
 from sklearn.ensemble import RandomForestRegressor, AdaBoostRegressor
 
 class Model_Trainer():
-    def __init__(self, data) -> None:
-        self.data = data
-        self.model = None
+    def __init__(self, model, train_data, test_data=None) -> None:
+        self.train_data = train_data
+        self.test_data = test_data
+        self.model = model
     
     def fit_data(self):
-        pass
-
-    def predict_data(self):
-        pass
-
-    def train_model(self):
-        pass
-
+        self.fit_model = self.model.fit(self.test_data)
+        return self.fit_model
+    
     def test_model(self):
-        pass
+        y = self.fit_model.predict(self.test_data)
+        return y
